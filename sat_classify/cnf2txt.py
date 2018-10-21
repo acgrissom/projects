737213c9cc77d1e3de68b6801fbfdc4017e8549e
Alvin Grissom II <alvin@Alvins-MBP.fios-router.home>
2018-10-20 22:01:30 -0400

    updating code and adding sat files

--- sat_classify/cnf2txt.py
+++ sat_classify/cnf2txt.py
@@ -14,11 +14,29 @@ def convert_to_text(filename):
         cleaned += ' '.join([x for x in line.split()[0:3]]) + '\n'
     return cleaned
 
-def convert_dir(in_dir, out_dir, sat_or_unsat):
+def convert_to_sparse_binary(filename, num_variables=50):
+    binary_encoding = ''
+    cleaned = convert_to_text(filename)
+    cleaned = cleaned.split('\n')
+    for line in cleaned:
+        cols = line.split()
+        new_cols = [str(0)] * (num_variables + 1)
+        for i in cols:
+            k = int(i)
+            new_cols[abs(k)] = str(1);
+            if k < 0:
+                new_cols[abs(k)] = str(-1);
+        binary_encoding += ' '.join(new_cols) + '\n'
+    return binary_encoding
+    
+def convert_dir(in_dir, out_dir, sat_or_unsat, encoding='sparse_binary'):
     converted = None
     for filename in os.listdir(in_dir):
         if filename.endswith(".cnf"):
-            converted = convert_to_text(in_dir + '/' + filename)
+            if encoding == 'dense_matrix':
+                converted = convert_to_text(in_dir + '/' + filename)
+            elif encoding == 'sparse_binary':
+                converted = convert_to_sparse_binary(in_dir + '/' + filename)
             print(filename)
             try:  
                 os.mkdir(out_dir)
@@ -34,5 +52,5 @@ def convert_dir(in_dir, out_dir, sat_or_unsat):
 
 #convert_dir('./CBS_k3_n100_m403_b10/','sat_files', 'sat')
 #convert_dir('./dubois/','sat_files', 'unsat')
-convert_dir('./UUF125.538.100','sat_files', 'unsat')
-convert_dir('./UF125.538.100','sat_files', 'sat')
+convert_dir('./data/uf50/unsat','sat_files', 'unsat')
+convert_dir('./data/uf50/sat','sat_files', 'sat')
--- sat_classify/keras_cnf.py
+++ sat_classify/keras_cnf.py
@@ -5,16 +5,20 @@ import numpy as np
 import sys
 import os
 import keras
-from keras.layers import Conv1D, Conv2D, Flatten, SimpleRNN, Reshape
+from keras.layers import Conv1D, Conv2D, Flatten, SimpleRNN, Reshape, LSTM, GRU, Dropout
 
-os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+#os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
 
 X = []
 Y = []
-NUM = 500
+NUM = 1000
 pos_num = 0
 neg_num = 0
-EXAMPLES_DIR='Downloads/sat_files'
+EXAMPLES_DIR='sat_files'
+NUM_CLAUSES = 220
+NUM_VARIABLES = 50
+
+shape = (NUM_CLAUSES, NUM_VARIABLES + 1)
 for filename in os.listdir(EXAMPLES_DIR):
     #sys.stderr.write(filename + '\n')
     if filename.endswith(".sat"):
@@ -23,15 +27,17 @@ for filename in os.listdir(EXAMPLES_DIR):
         pos_num += 1
         example = np.loadtxt(EXAMPLES_DIR + '/' + filename, delimiter=' ', skiprows=0)
         #example = np.asmatrix(example)
-        example = np.resize(example,(430,3))
+        #example = np.resize(example,(430,3))
         X.append(example)
+        #print(example.shape)
+        shape = example.shape
         #X = np.append(X, example, axis=0)
         Y.append(1)
     elif filename.endswith(".unsat"):
         if neg_num >= NUM:
             continue 
         example = np.loadtxt(EXAMPLES_DIR + '/' + filename, delimiter=' ', skiprows=0)
-        example = np.resize(example,(430,3))
+        #example = np.resize(example,(430,3))
         #example = np.asmatrix(X, example, axis=0)        
         neg_num += 1
         X.append(example)
@@ -64,19 +70,20 @@ model = Sequential()
 #input dimension should be same as the number of features
 
 
-model.add(Reshape((430,3,1)))
-#model.add(Conv2D(64, (3, 1), padding='same'))
-
+model.add(Reshape((NUM_CLAUSES, NUM_VARIABLES + 1, 1)))
+model.add(Conv2D(64, (50, 50), padding='same'))
+model.add(Dropout(0.3))
 model.add(Flatten())
 
-model.add(Dense(32, init='uniform', activation='relu'))
+#model.add(LSTM(32, init='uniform', activation='relu'))
+#model.add(Dense(32, init='uniform', activation='relu'))
 
 model.add(Dense(1, init='uniform', activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 #Train the model for 200 epochs
-model.fit(X_train, Y_train, validation_data=(X_test, Y_test), nb_epoch=100, batch_size=5)
+model.fit(X_train, Y_train, validation_data=(X_test, Y_test), nb_epoch=100, batch_size=20)
 
 #Look at the results
 scores = model.evaluate(X_test, Y_test)
