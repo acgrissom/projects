84bf532e56ebe9a9b93effad60814dba3ae9b59a
Bailey Lin <baileylinhfi@gmail.com>
2022-11-29 17:42:38 +0000

    update

--- stylegan3/regression.py
+++ stylegan3/regression.py
@@ -1,10 +1,32 @@
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import torchvision
+import glob
+import torch
 
-def main():
+
+def plot_regression():
     data = pd.read_csv("correct_LAB_format_images_data.csv")
-    sns.lmplot(data=data, x="luminance",y="scores_rgb")
-    plt.savefig("regression.png")
-if __name__=="__main__":
-    main()
+    sns.lmplot(data=data, x="luminance",y="scores_rgb", fit_reg=True, 
+               line_kws={"color":"red"}, scatter_kws={'alpha':0.3, "color":"green"})
+    plt.savefig("regression.svg", format="svg")
+
+def get_average_color():
+    df = pd.read_csv("correct_LAB_format_images_data.csv")
+    image_ids = df["image_id"]
+    r = []
+    g = []
+    b = []
+    for id in image_ids:
+        file = glob.glob("ffhq_images_1024x1024/*/"+id)[0]
+        image = torchvision.io.read_image(file)
+        image  = image.float()
+        red_mean, green_mean, blue_mean = torch.mean(image,dim=[1,2]).numpy()
+        r.append(red_mean)
+        g.append(green_mean)
+        b.append(blue_mean)
+    df["red_mean"] = r
+    df["green_mean"] = g
+    df["blue_mean"] = b
+    df.to_csv("correct_LAB_format_images_data.csv")
