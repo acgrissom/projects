41959f793a34a70976d80a3f4630749b71063aef
Bailey Lin <ylin5@wren.localdomain>
2022-12-06 13:30:18 -0500

    update

--- stylegan3/regression.R
+++ stylegan3/regression.R
@@ -1,13 +1,16 @@
+setwd("/Users/BaileyLin/Desktop")
 data=read.csv("correct_LAB_format_images_data.csv",header=TRUE)
 attach(data)
-x1 = scores_rgb
+x1 = luminance
 x2 = red_mean
 x3 = green_mean
 x4 = blue_mean
-y = luminance
+y = scores_rgb
 model = lm(y~x1+x2+x3+x4)
 summary(model)
-install.packages("car",repo = "https://lib.ugent.be/CRAN")
 library(car)
 vif(model)
-
+par(mfrow=c(2,2))
+plot(model)
+pairs(y~x1+x2+x3+x4)
+mmps(model)
