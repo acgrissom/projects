027ce52a2405b8b887a283f6b0f4e580da026d27
Bailey Lin <ylin5@wren.localdomain>
2022-12-06 12:59:54 -0500

    update regression

--- stylegan3/regression.R
+++ stylegan3/regression.R
@@ -1,4 +1,13 @@
-data=read.csv("correct_LAB_format_images_data.csv")
-names(data)
-
+data=read.csv("correct_LAB_format_images_data.csv",header=TRUE)
+attach(data)
+x1 = scores_rgb
+x2 = red_mean
+x3 = green_mean
+x4 = blue_mean
+y = luminance
+model = lm(y~x1+x2+x3+x4)
+summary(model)
+install.packages("car",repo = "https://lib.ugent.be/CRAN")
+library(car)
+vif(model)
 
