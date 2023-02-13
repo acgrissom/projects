b2e968555820b9d7eab2739856e37a346a9b4565
Alvin Grissom II <acgrissom@gmail.com>
2023-02-13 17:21:17 -0500

    update prereqs script

--- ganfaces/scripts/install_r_prereqs.R
+++ ganfaces/scripts/install_r_prereqs.R
@@ -1,3 +1,3 @@
 install.packages(c("lme4","readr","tidyr","effects","ggplot2","psych","MASS",
        "Rmisc","plyr","dplyr","lmerTest","ggthemes","lsmeans",
-       "pastecs","sjstats","car","readxl","ggdist, svglite"),repos = "http://cran.us.r-project.org")
+       "pastecs","sjstats","car","readxl","ggdist, svglite", "ggdist"),repos = "http://cran.us.r-project.org", dependencies = TRUE)
