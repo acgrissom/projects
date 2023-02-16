1f44a436bde78c4148c3a36d392b3d6d1741694d
Alvin Grissom II <alvin@Alvins-MacBook-Air.local>
2023-02-16 02:28:00 -0500

    new figures code, updated makefile, svg plots

--- ganfaces/scripts/AtypicalFacePlotCode.R
+++ ganfaces/scripts/AtypicalFacePlotCode.R
@@ -1,6 +1,6 @@
 lib<-c("lme4","readr","tidyr","effects","ggplot2","psych","MASS",
        "Rmisc","plyr","dplyr","lmerTest","ggthemes","lsmeans",
-       "pastecs","sjstats","car","readxl","ggdist", "svglite")
+       "pastecs","sjstats","car","readxl","ggdist", "svglite","gghdr")
 lapply(lib,require,character.only=TRUE)
 
 gan_hair<-read.csv("data/AtypicalFaceData.csv")
@@ -9,25 +9,48 @@ mean_gan_hair<- summarySE(data=gan_hair, measurevar = "discriminator_score",
                           groupvars = c("race","hair_length"),
                           na.rm = FALSE, conf.interval = 0.95)
                           
-plot_hair<- ggplot(gan_hair, aes(x=discriminator_score, fill = hair_length)) +
-  geom_density(alpha = 0.25)+
+race_hair<- ggplot(gan_hair, aes(x=discriminator_score, fill = hair_length)) +
+    geom_density(alpha = 0.25)+
     facet_grid(race ~ .) +
-  theme_minimal() +
+    theme_minimal() +
     theme(strip.text.y = element_text(size = 14),
-        legend.position="bottom",
-        axis.text=element_text(size=12),
-        axis.title=element_text(size=14),
-        legend.title=element_text(size=12),
-        #legend.title=element_blank(),
-        legend.text=element_text(size=12)) +
+          legend.position="bottom",
+          axis.text=element_text(size=12),
+          axis.title=element_text(size=14),
+          legend.title=element_text(size=12),
+          #legend.title=element_blank(),
+          legend.text=element_text(size=12)) +
         scale_color_colorblind() +
   labs(y = "Density", x = "Score",
        fill = "Hair") 
 
 
-plot_hair
-ggsave("results/figures/atypical_mens_face_dist.svg", plot=plot_hair)
-ggsave("results/figures/atypical_mens_face_dist.jpg", plot=plot_hair)
+ 
+
+
+ggsave("results/figures/atypical_mens_face_dist.svg", plot=race_hair)
+ggsave("results/figures/atypical_mens_face_dist.jpg", plot=race_hair)
+
+
+plot_hair<- ggplot(gan_hair, aes(x=discriminator_score, fill = hair_length)) +
+    geom_density(alpha = 0.25) +
+    theme_minimal() +
+    theme(strip.text.y = element_text(size = 14),
+          legend.position="bottom",
+          axis.text=element_text(size=12),
+          axis.title=element_text(size=14),
+          legend.title=element_text(size=12),
+          #legend.title=element_blank(),
+          legend.text=element_text(size=12)) +
+        scale_color_colorblind() +
+  labs(y = "Density", x = "Score",
+       fill = "Hair") 
+
+
+ggsave("results/figures/atypical_mens_face_dist_aggregate.svg", plot=plot_hair)
+ggsave("results/figures/atypical_mens_face_dist_aggregate.jpg", plot=plot_hair)
+
+
 
 
 plot_hair<- ggplot(gan_hair, aes(x=discriminator_score, fill = hair_length)) +
@@ -45,9 +68,31 @@ plot_hair<- ggplot(gan_hair, aes(x=discriminator_score, fill = hair_length)) +
        fill = "Hair") 
 
 
+
 plot_hair
 ggsave("results/figures/atypical_mens_face_hist.svg", plot=plot_hair)
 ggsave("results/figures/atypical_mens_face_hist.jpg", plot=plot_hair)
+
+
+plot_race<- ggplot(gan_hair, aes(x=discriminator_score, fill = race)) +
+    geom_density(alpha = 0.25) +
+    theme_minimal() +
+    theme(strip.text.y = element_text(size = 14),
+          legend.position="bottom",
+          axis.text=element_text(size=12),
+          axis.title=element_text(size=14),
+          legend.title=element_text(size=12),
+          #legend.title=element_blank(),
+          legend.text=element_text(size=12)) +
+        scale_color_colorblind() +
+  labs(y = "Density", x = "Score",
+       fill = "Race") 
+
+
+ggsave("results/figures/atypical_mens_face_race_dist_aggregate.svg", plot=plot_race)
+ggsave("results/figures/atypical_mens_face_race_dist_aggregate.jpg", plot=plot_race)
+
+
 
 
 plot_hair_raincloud <- ggplot(gan_hair, aes(hair_length, discriminator_score)) + 
---
+++ ganfaces/scripts/luminance_hdr.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+library(ggdensity)
+data=read.csv("data/correct_LAB_format_images_data.csv",header=TRUE)
+attach(data)
+x1 = luminance
+x2 = red_mean
+x3 = green_mean
+x4 = blue_mean
+y = scores_rgb
+theme_set(theme_minimal())
+p_points <- ggplot(data, aes(data, x=luminance, y=scores_rgb)) +
+    xlab("Luminance") + ylab("Score") +   labs(fill='HDR') +
+    geom_point() + theme(legend.position = "bottom", legend.title = element_text("HDR")) 
+
+p_hdr_points <- ggplot(data, aes(x=luminance, y=scores_rgb, xlab="Luminance", ylab="Score")) +
+    xlab("Luminance") + ylab("Score") +  labs(fill='HDR') +
+    geom_hdr_points() + theme(legend.position = "bottom", legend.title = element_text("HDR")) +
+    geom_smooth(method='lm', se=FALSE) 
+
+#p_points + p_hdr_points
+ggsave("results/figures/luminance_hdr.svg")
+ggsave("results/figures/luminance_hdr.jpg")
