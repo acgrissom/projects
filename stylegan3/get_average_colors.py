7cb17d9e61fb84c398c5acfe9412d428cdf254aa
Alvin Grissom II <acgrissom@gmail.com>
2023-06-12 03:55:06 +0000

    update makefile to generate color data for all official models

--- stylegan3/get_average_colors.py
+++ stylegan3/get_average_colors.py
@@ -14,7 +14,7 @@ import click
 import sys
 import argparse
 import time
-
+from torchvision.transforms.functional import get_image_size
 
 def get_discriminator(gan_model_filename):
     with open(gan_model_filename, 'rb') as f:
@@ -29,11 +29,14 @@ def get_average_colors(image_filename):
     return red_mean, green_mean, blue_mean
 
 
-def get_score(image_filename, discriminator, resize=None):
+def get_score(image_filename, discriminator, model_image_size, antialias=False):
     D = discriminator
     pic = torchvision.io.read_image(image_filename).cuda()
-    if resize is not None:       
-        pic = torchvision.transforms.functional.resize(pic, resize, antialias=False)
+    image_size = get_image_size(pic)
+    if image_size != model_image_size or antialias == True:       
+        pic = torchvision.transforms.functional.resize(pic, model_image_size, antialias=antialias)
+        
+        
     score = D(pic[None,:,:,:],c=None).cuda()
     score = score.item()
     return score
@@ -48,7 +51,7 @@ def get_luminance(image_filename):
     return mean
 
 
-def process_images(images_dir, discriminator, out_csv_filename, resize=None):
+def process_images(images_dir, discriminator, out_csv_filename, model_image_size):
     D = discriminator
     folders = os.listdir(images_dir)
     folders = [f for f in folders if os.path.isdir(images_dir + "/" + f)]
@@ -62,7 +65,7 @@ def process_images(images_dir, discriminator, out_csv_filename, resize=None):
             images[file] = {}
             image_absolute_path = images_dir + "/" + folder + "/" + file
             pic = torchvision.io.read_image(image_absolute_path).cuda()
-            score = get_score(image_absolute_path, D, resize)
+            score = get_score(image_absolute_path, D, model_image_size)
             images[file]["scores_rgb"] = score
             r, g, b = get_average_colors(image_absolute_path)
             images[file]["red_mean"] = r
@@ -81,14 +84,20 @@ def main():
     parser = argparse.ArgumentParser(description='Generate CSV file with RGB and luminance values.')
     parser.add_argument('--images_dir', type=str,help="root directory of processed images")
     parser.add_argument('--dest_csv', type=str, help="full path and filename of the destination CSV file.")
-    parser.add_argument('--gan_model', type=str, help="file name of GAN model to use")
-    parser.add_argument('--resize', type=int, nargs=1, required=False, default=None,help="Optionally resize images to this height while maintaining aspect ratio..  For example: --resize 1024")
+    parser.add_argument('--gan_model', type=str, help="file name of GAN model to use.  Assumes last part of name (before .pkl) is the width and height of the images in the modal, e.g., my_model256x256.pkl.")
+    #parser.add_argument('--resize', type=int, nargs=1, required=False, default=None,help="Optionally resize images to this height while maintaining aspect ratio..  For example: --resize 1024")
+    
     args = parser.parse_args()
     images_dir = args.images_dir
     discriminator = get_discriminator(args.gan_model)
     out_csv_filename = args.dest_csv
-    resize = args.resize
-    process_images(images_dir, discriminator, out_csv_filename, resize=resize)
+    #resize = args.resize
+    ### find dimensions use by model for resizing
+    model_prefix = os.path.splitext(args.gan_model)[0]
+    model_image_height = int(model_prefix.split('x')[1])
+    dimensions = (model_image_height, model_image_height)
+    
+    process_images(images_dir, discriminator, out_csv_filename, model_image_height)
     end_time = time.time()
     elapsed_time = (end_time - start_time) / 60.0
     print(f"Took {elapsed_time} minutes.")
